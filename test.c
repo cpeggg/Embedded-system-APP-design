63e06cabc22bce4924ce8d02a59423c644331116
cpegg <cpeggg@gmail.com>
2018-04-13 00:41:32 +0800

    Update test.c

--- test.c
+++ test.c
@@ -3,7 +3,7 @@ int a(){
   return 1;
 }
 /*
-  Please upload one function to this file so that we can finish this program.
+  Please upload one function to this file using git command so that we can finish this program.
 */
 int main(){
   int i;
