4d496f7fc626cfd43ab2683a8ae1e736a3dd3752
cpeggg <594076223@qq.com>
2018-04-19 13:16:19 +0800

    file_update

--- app/src/main/java/priv/valueyouth/rhymemusic/activity/MainActivity.java
+++ app/src/main/java/priv/valueyouth/rhymemusic/activity/MainActivity.java
@@ -4,22 +4,21 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatDelegate;
-import android.util.Log;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import priv.valueyouth.rhymemusic.R;
@@ -143,11 +142,6 @@ public class MainActivity extends BaseActivity
 
                 break;
 
-            case R.id.motion_analysis:
-                Intent intent = new Intent(MainActivity.this, MotionAnalysis.class);
-                startActivity(intent);
-                break;
-
             case R.id.nav_read_audio:
                 String str = "因技术原因，该功能暂未实现。";
                 Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
@@ -182,6 +176,11 @@ public class MainActivity extends BaseActivity
             case R.id.nav_about:
                 Intent inAbout = new Intent(MainActivity.this, AboutActivity.class);
                 startActivity(inAbout);
+                break;
+
+            case R.id.motion_analysis:
+                Intent inMotion = new Intent(MainActivity.this, MotionAnalysis.class);
+                startActivity(inMotion);
                 break;
 
             case R.id.nav_quit:
--- app/src/main/java/priv/valueyouth/rhymemusic/activity/MotionAnalysis.java
+++ app/src/main/java/priv/valueyouth/rhymemusic/activity/MotionAnalysis.java
@@ -1,19 +1,38 @@
 package priv.valueyouth.rhymemusic.activity;
 
-import android.graphics.Camera;
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import priv.valueyouth.rhymemusic.R;
-import priv.valueyouth.rhymemusic.application.MusicApplication;
-import priv.valueyouth.rhymemusic.util.Audio;
-import priv.valueyouth.rhymemusic.util.AudioUtil;
 
 
-public class MotionAnalysis extends AppCompatActivity {
+public class MotionAnalysis extends AppCompatActivity
+{
 
     private SurfaceView sv_takephoto;
     private Button b_OK;
@@ -23,7 +42,7 @@ public class MotionAnalysis extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_takephoto);
+        setContentView(R.layout.emotion_analysis);
         //获得SurfaceView的ID
         sv_takephoto =(SurfaceView) findViewById(R.id.sv_takephoto);
         //获得拍照的ID
@@ -75,19 +94,149 @@ public class MotionAnalysis extends AppCompatActivity {
                     public void onPictureTaken(byte[] bytes, Camera camera) {
                         //将字节数组转成图片
                         Bitmap bitmap= BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+                        String filename=System.currentTimeMillis()+".jpg";
+                        String picpath="/mnt/sdcard/DCIM/"+filename;
                         try {
-                            FileOutputStream fos=new FileOutputStream("/mnt/sdcard/DCIM/"+System.currentTimeMillis()+".jpg");
+                            FileOutputStream fos=new FileOutputStream(picpath);
                             bitmap.compress(Bitmap.CompressFormat.PNG,85,fos);
                             camera.stopPreview();
                             camera.startPreview();
                         } catch (FileNotFoundException e) {
                             e.printStackTrace();
                         }
+                        //--------------------
+                        //upload the picture
+                        upload_picture(picpath,filename);
                     }
                 });
 
             }
+            @SuppressLint("StaticFieldLeak")
+            private void upload_picture(final String picpath,final String filename) {
+                new AsyncTask<String, Void, Object>() {
+
+                    //在doInBackground 执行完成后，onPostExecute 方法将被UI 线程调用，
+                    // 后台的计算结果将通过该方法传递到UI 线程，并且在界面上展示给用户.
+                    protected void onPostExecute(Object result) {
+                        super.onPostExecute(result);
+                        String tips = "情绪识别结果："+result;
+                        Toast.makeText(MotionAnalysis.this, tips, Toast.LENGTH_SHORT).show();
+                        return ;
+                    }
+
+                    //该方法运行在后台线程中，因此不能在该线程中更新UI，UI线程为主线程
+                    protected Object doInBackground(String... params) {
+                        String result = uploadpic(picpath,filename);
+                        return result;
+                    }
+
+                }.execute();
+
+            }
+
         });
 
+    }
+    private String uploadpic(String picpath,String filename){
+
+        Log.d("INFO","upload the picture");
+        //--------------------
+
+        HttpURLConnection conn = null;
+
+        /// boundary就是request头和上传文件内容的分隔符(可自定义任意一组字符串)
+        String BOUNDARY = "******";
+        // 用来标识payLoad+文件流的起始位置和终止位置(相当于一个协议,告诉你从哪开始,从哪结束)
+        String preFix = ("\r\n--" + BOUNDARY + "--\r\n");
+
+        try {
+            // (HttpConst.uploadImage 上传到服务器的地址
+
+            URL url = new URL("https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize");
+            conn = (HttpURLConnection) url.openConnection();
+            Log.d("INFO","Connection built.");
+            conn.setConnectTimeout(5000);
+            conn.setReadTimeout(30000);
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setUseCaches(false);
+            // 设置请求方法
+            conn.setRequestMethod("POST");
+            // 设置header
+            conn.setRequestProperty("Content-Type","octet-stream");
+            conn.setRequestProperty("Ocp-Apim-Subscription-Key","154ff89e136d41cd80d7ad5d3fe9eae5");
+
+//            conn.setRequestProperty("Accept","*/*");
+//            conn.setRequestProperty("Connection", "keep-alive");
+//            conn.setRequestProperty("Ocp-Apim-Subscription-Key","154ff89e136d41cd80d7ad5d3fe9eae5");
+//            conn.setRequestProperty("Content-Type",
+//                    "multipart/form-data; boundary=" + BOUNDARY);
+            // 获取写输入流
+            OutputStream out = new DataOutputStream(conn.getOutputStream());
+            // 获取上传文件
+            File file = new File(picpath);
+            Log.d("INFO","FILE got.");
+
+            // 获取文件流
+            FileInputStream fileInputStream = new FileInputStream(file);
+            DataInputStream inputStream = new DataInputStream(fileInputStream);
+
+            // 每次上传文件的大小(文件会被拆成几份上传)
+            int picbytes;
+            // 每次上传的大小
+            byte[] bufferOut = new byte[1024];
+            // 上传文件
+            Log.d("INFO","Start to upload files");
+            while ((picbytes = inputStream.read(bufferOut)) != -1) {
+                // 上传文件(一份)
+                out.write(bufferOut, 0, picbytes);
+            }
+            Log.d("INFO","Ready to close inputStream");
+            // 关闭文件流
+            inputStream.close();
+
+            // 标识payLoad + 文件流的结尾位置
+            out.write(preFix.getBytes());
+
+            // 至此上传代码完毕
+
+            // 总结上传数据的流程：preFix + payLoad(标识服务器表单接收文件的格式) + 文件(以流的形式) + preFix
+            // 文本与图片的不同,仅仅只在payLoad那一处的后缀的不同而已。
+
+            // 输出所有数据到服务器
+            out.flush();
+
+            // 关闭网络输出流
+            out.close();
+            Log.d("INFO","GET request from server.");
+            // 重新构造一个StringBuffer,用来存放从服务器获取到的数据
+            strBuf = new StringBuffer();
+
+            // 打开输入流 , 读取服务器返回的数据
+            BufferedReader reader = new BufferedReader(new
+                    InputStreamReader(conn.getInputStream()));
+
+            String line;
+
+            // 一行一行的读取服务器返回的数据
+            while ((line = reader.readLine()) != null) {
+                strBuf.append(line).append("\n");
+            }
+
+            // 关闭输入流
+            reader.close();
+
+            Log.d("INFO","UPLOAD SUCCESS");
+
+            if (conn != null) {
+                conn.disconnect();
+            }
+
+            // 打印服务器返回的数据logD("上传成功:"+strBuf.toString());
+            return strBuf.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "ERROR";
+        }
     }
 }
